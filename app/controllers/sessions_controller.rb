0239ec28705e1fd681efd53cc94fe81a0a1aa505
Graham Troyer-Joy <grahamtroyerjoy@gmail.com>
2018-06-18 00:21:57 -0400

    passwording

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -5,9 +5,23 @@ class SessionsController < ApplicationController
   end
 
   def create
+    @user = User.find_by(username: session_params[:username])
+    if @user && @user.authenticate(session_params[:password])
+      redirect_to "/"
+    else
+      @user ||= User.new(username: session_params[:username])
+      @errors = ["Those credentials don't match anything we have in the database"]
+      render :new
+    end
   end
 
   def destroy
+
+  end
+
+  private
+  def session_params
+    params.require(:user).permit(:username, :password)
   end
 
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,8 +1,20 @@
 class User < ApplicationRecord
 
-  attr_accessor :password
+  attr_reader :password
   has_many :entries, dependent: :destroy, foreign_key: :author_id
 
   validates :name, :username, :email, :password, presence: true
   validates :username, :email, uniqueness: true
+
+
+  def password=(value)
+    @password = value
+    self.password_digest = BCrypt::Password.create(value)
+  end
+
+
+  def authenticate(password)
+    return BCrypt::Password.new(password_digest) == password
+  end
+
 end
